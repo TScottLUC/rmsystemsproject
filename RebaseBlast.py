ab2332455986ddc053290fc08886965184a1f4be
Thomas Scott <tscott3@luc.edu>
2021-03-26 12:31:26 -0500

    RebaseBlast script now writes information for the top hit to output file and can run multiple files from the same directory

--- RebaseBlast.py
+++ RebaseBlast.py
@@ -12,33 +12,58 @@ from Bio import SeqIO
 
 #local blast DB is named LocalRebaseDB
 
-input_file='shortened.fasta' #EXAMPLE FILE
-output_file='temp.csv'
-
 sequences='formattedSeqs.fasta'
+print("Reading REBASE sequences from " + sequences + "...")
 records=list(SeqIO.parse(sequences,'fasta'))
-final_output='output.txt'
 
-#This blast command will generate a csv formatted output file containing the Query Seq-id, the subject seq-id, the e-value, the query coverage, the percent identity, the bitscore, and the alignment length for each hit.
-blast_command = 'blastn -query ' + input_file + ' -db LocalRebaseDB -out ' + output_file + ' -outfmt "10 qseqid sseqid evalue qcovs pident bitscore length"'
-os.system(blast_command)
+output_name = "output.txt"
+final_output=open(output_name,'w')
+fileCount = 0
 
-#read the csv file and sort it by bitscore
-with open(output_file) as csvFile:
-  reader = csv.reader(csvFile, delimiter=',')
-  sortedList = sorted(reader,key=lambda x: float(x[5]),reverse=True) #blast output is sorted by bitscore 
+for fileName in os.listdir("sequences"):
 
-topHit=sortedList[0] #now we have just the top hit for the multi-fasta assembly
+  temp_output_file='temp.csv'
+  
+  fileCount += 1
+  
+  print("Running " + fileName + "..." + "(" + str(fileCount) + ")")
+  
+  #This blast command will generate a csv formatted output file containing the Query Seq-id, the subject seq-id, the e-value, the query coverage, the percent identity, the bitscore, and the alignment length for each hit.
+  blast_command = 'blastn -ungapped -query sequences/' + fileName + ' -db LocalRebaseDB -out ' + temp_output_file + ' -outfmt "10 qseqid sseqid evalue qcovs pident bitscore length"'
+  os.system(blast_command)
+  
+  #read the csv file and sort it by bitscore
+  with open(temp_output_file) as csvFile:
+    reader = csv.reader(csvFile, delimiter=',')
+    sortedList = sorted(reader,key=lambda x: float(x[5]),reverse=True) #blast output is sorted by bitscore 
+  
+  topHit=sortedList[0] #now we have just the top hit for the multi-fasta assembly
+  
+  topHitName=topHit[1]
+  for record in records:
+    if topHitName == record.id:
+      description = record.description
+      topHitSequence = str(record.seq)
+      
+  hitInfo = description.split("\t")
+  hitDict = {}
+  for info in hitInfo:
+    hitDict[info[:info.index(":")]] = info[info.index(":")+1:]
+  
+  genomeName=fileName #genome name
+  contig=topHit[0] #contig
+  nameOfSystem = hitDict["REBASE"]
+  systemType = hitDict["EnzType"]
+  
+  final_output.write("Genome file: " + genomeName + "\n")
+  final_output.write("Contig: " + contig + "\n")
+  final_output.write("Name of System: " + nameOfSystem + "\n")
+  final_output.write("System Type: " + systemType + "\n")
+  final_output.write(topHitSequence + "\n\n")
 
-topHitName=topHit[1]
-for record in records:
-  if topHitName == record.id:
-    description = record.description
-
-genomeName=input_file
-contig=topHit[0]
-
-
+print("Cleaning up...")
+os.system("rm temp.csv")
+print("Done! Output can be found in " + output_name + ".")
 #output needed: Genome name - assembly file name
 #contig - provided by qseqid in csv file
 #name of system - should be provided in top hit name
